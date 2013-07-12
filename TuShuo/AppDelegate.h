d1c4015a80c4581308bd1cf5f10eae07e8a39603
ihj <ihj@ihjs-Mac.local>
2013-07-12 08:52:18 -0700

    Initial Commit

---
+++ TuShuo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TuShuo
+//
+//  Created by ihj on 7/12/13.
+//  Copyright (c) 2013 ihj.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
